b57e1f57e9e3af1ef1be172947874e6791909b4a
WoeunJung <leena09123@naver.com>
2024-07-13 22:26:39 +0900

    Init wasin's backend project

---
+++ wasin/src/main/java/com/wasin/backend/WasinApplication.java
@@ -0,0 +1,13 @@
+package com.wasin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasinApplication.class, args);
+    }
+
+}
---
+++ wasin/src/test/java/com/wasin/backend/WasinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
