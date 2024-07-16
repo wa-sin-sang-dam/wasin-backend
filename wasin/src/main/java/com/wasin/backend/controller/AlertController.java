5118489b6efab2a5d537f54e958ec49e38da9c67
WoeunJung <leena09123@naver.com>
2024-07-16 19:25:34 +0900

    feat: implement ssh test

--- wasin/src/main/java/com/wasin/backend/controller/AlertController.java
+++ wasin/src/main/java/com/wasin/backend/controller/AlertController.java
@@ -1,5 +1,6 @@
 package com.wasin.backend.controller;
 
+import com.jcraft.jsch.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,59 @@ public class AlertController {
     @PostMapping
     public ResponseEntity<String> receive() {
         log.debug("success");
+        ssh();
         return ResponseEntity.ok("success");
+    }
+
+    private void ssh() {
+        String host = "219.241.29.68";
+        int    port = 1222;
+        String username = "root";
+        String password = "pica1104";
+
+        System.out.println("==> Connecting to" + host);
+        Session session = null;
+        Channel channel = null;
+
+        // 2. 세션 객체를 생성한다 (사용자 이름, 접속할 호스트, 포트를 인자로 준다.)
+        try {
+            // 1. JSch 객체를 생성한다.
+            JSch jsch = new JSch();
+            session = jsch.getSession(username, host, port);
+
+            // 3. 패스워드를 설정한다.
+            session.setPassword(password);
+
+            // 4. 세션과 관련된 정보를 설정한다.
+            java.util.Properties config = new java.util.Properties();
+            // 4-1. 호스트 정보를 검사하지 않는다.
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+
+            // 5. 접속한다.
+            session.connect();
+            System.out.println("접속 완료");
+
+            // 6. sftp 채널을 연다.
+            channel = session.openChannel("exec");
+
+            // 8. 채널을 SSH용 채널 객체로 캐스팅한다
+            ChannelExec channelExec = (ChannelExec) channel;
+
+            channelExec.setCommand("touch awstext1.txt");
+            channelExec.connect();
+            System.out.println("성공");
+
+        } catch (JSchException e) {
+            System.out.println("fail");
+            e.printStackTrace();
+        } finally {
+            if (channel != null) {
+                channel.disconnect();
+            }
+            if (session != null) {
+                session.disconnect();
+            }
+        }
     }
 }
