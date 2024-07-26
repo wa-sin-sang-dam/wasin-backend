91706216bc262a3e0623b884929e6a0f955e64ac
WoeunJung <leena09123@naver.com>
2024-07-27 03:04:49 +0900

    feat: change common url path for alert, metric

--- wasin/src/main/java/com/wasin/backend/controller/MetricController.java
+++ wasin/src/main/java/com/wasin/backend/controller/MetricController.java
@@ -1,5 +1,6 @@
 package com.wasin.backend.controller;
 
+import com.wasin.backend._core.util.ApiUtils;
 import com.wasin.backend.service.MetricService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/metric")
+@RequestMapping("/router")
 public class MetricController {
 
     private final MetricService metricService;
 
-    @GetMapping
-    public ResponseEntity<String> getGrafanaDashboard() {
+    @GetMapping("/monitoring")
+    public ResponseEntity<?> getGrafanaDashboard() {
         String response = metricService.getGrafanaDashboard();
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(ApiUtils.success(response));
     }
 }
