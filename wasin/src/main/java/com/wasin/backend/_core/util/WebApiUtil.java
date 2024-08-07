02b8923539846ca32041ec431074137d11907a52
1jeongg <leena0912@pusan.ac.kr>
2024-08-07 20:10:40 +0900

    feat: add methods for calling grafana, prometheus server

--- wasin/src/main/java/com/wasin/backend/_core/util/WebApiUtil.java
+++ wasin/src/main/java/com/wasin/backend/_core/util/WebApiUtil.java
@@ -3,6 +3,8 @@ package com.wasin.backend._core.util;
 import com.wasin.backend._core.exception.BaseException;
 import com.wasin.backend._core.exception.error.ServerException;
 import com.wasin.backend.domain.dto.CompanyDTO;
+import com.wasin.backend.domain.dto.RouterResponse;
+import com.wasin.backend.domain.entity.Router;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,7 @@ import org.springframework.web.util.UriBuilder;
 
 import java.net.URI;
 import java.net.URLEncoder;
+import java.util.List;
 import java.util.function.Function;
 
 @Slf4j
@@ -26,6 +29,12 @@ public class WebApiUtil {
 
     @Value("${security.open-api.token}")
     String apiToken;
+
+    @Value("${jang.header}")
+    String grafanaHeader;
+
+    @Value("${jang.datasource-uid}")
+    static String grafanaDatasourceUID;
 
     public CompanyDTO.ResponseValue getCompanyList(String name, Long page) {
         try {
@@ -45,6 +54,65 @@ public class WebApiUtil {
             log.debug(e.getMessage());
             throw new ServerException(BaseException.COMPANY_OPEN_API_FAIL);
         }
+    }
+
+    public RouterResponse.prometheusRouter getPrometheusRouter() {
+        try {
+            return webClient.mutate()
+                    .build()
+                    .get()
+                    .uri("http://prometheus.daily-cotidie.com/api/v1/series?match[]=wifi_network_quality")
+                    .retrieve()
+                    .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(),
+                            clientResponse -> clientResponse
+                                    .bodyToMono(String.class)
+                                    .map(body -> new ServerException(BaseException.COMPANY_OPEN_API_FAIL)))
+                    .bodyToMono(RouterResponse.prometheusRouter.class)
+                    .block();
+        } catch(Exception e) {
+            log.debug(e.getMessage());
+            throw new ServerException(BaseException.PROMETHUS_SERVER_FAIL);
+        }
+    }
+
+    public String getRouterState(Router router) {
+        try  {
+            RouterResponse.Queries requestDTO = gerRequestDTO(router);
+            long value = webClient.mutate()
+                    .build()
+                    .post()
+                    .uri("http://grafana.daily-cotidie.com/api/ds/query")
+                    .bodyValue(requestDTO)
+                    .header("Authorization", "Bearer " + grafanaHeader)
+                    .retrieve()
+                    .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(),
+                            clientResponse -> clientResponse
+                                    .bodyToMono(String.class)
+                                    .map(body -> new ServerException(BaseException.COMPANY_OPEN_API_FAIL)))
+                    .bodyToMono(RouterResponse.RouterResult.class)
+                    .block()
+                    .results().A().frames().get(0).data().values().get(1).get(0);
+
+            log.debug("개수 " + value);
+            if (value <= 10) return "쾌적";
+            else return "포화";
+
+        } catch(Exception e) {
+            log.debug(e.getMessage());
+            throw new ServerException(BaseException.GRAFANA_SERVER_FAIL);
+        }
+    }
+
+    private static RouterResponse.Queries gerRequestDTO(Router router) {
+        String expr = "sum(wifi_stations{instance=\"" + router.getInstance() + "\", job=\"" +  router.getJob() + "\"})";
+        String type = "prometheus";
+        String refId = "A";
+        RouterResponse.Queries requestDTO = new RouterResponse.Queries(
+                List.of(new RouterResponse.Query(refId, expr, true, new RouterResponse.Datasource(type, grafanaDatasourceUID))),
+                "now-5m",
+                "now"
+        );
+        return requestDTO;
     }
 
     private Function<UriBuilder, URI> getURI(String name, Long page) {
