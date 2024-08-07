f776fe9fdd6a0afaa2e5e38b5fc2d97b5901b276
1jeongg <leena0912@pusan.ac.kr>
2024-08-07 21:26:19 +0900

    chore: add uid environment variable and add datas for test

--- wasin/src/main/resources/db/teardown.sql
+++ wasin/src/main/resources/db/teardown.sql
@@ -4,6 +4,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS user_tb;
 DROP TABLE IF EXISTS company_tb;
 DROP TABLE IF EXISTS company_image_tb;
+DROP TABLE IF EXISTS router_tb;
 
 create table user_tb (
      is_mode_auto bit,
@@ -31,7 +32,22 @@ create table company_image_tb (
       company_image_id bigint not null auto_increment,
       company_id bigint,
       url varchar(1000),
+      width int,
+      height int,
       primary key (company_image_id)
+);
+
+create table router_tb (
+    router_id bigint not null auto_increment,
+    company_id bigint,
+    position_x float(53),
+    position_y float(53),
+    instance varchar(255),
+    job varchar(255),
+    mac_address varchar(255),
+    router_name varchar(255),
+    ssid varchar(255),
+    primary key (router_id)
 );
 
 SET FOREIGN_KEY_CHECKS = 1;
@@ -45,7 +61,12 @@ INSERT INTO user_tb (`user_id`, `username`,`email`,`password`,`lock_password`, `
 (1, '1jeongg', 'leena0912@naver.com', '{bcrypt}$2a$10$ZyY8A2GMb5a9tq5RD1/LsOc7ExG1VX9KAWIsWOpojnlK92mQEOMZC', '{bcrypt}$2a$10$NlF1LONeXtejcNReYu3XPu37FJrNll691IdoUykpY89aUzgwlDlYK', 'SUPER_ADMIN', 'ACTIVE', true, 1),
 (2, '2jeongg', 'leena0913@naver.com', '{bcrypt}$2a$10$ZyY8A2GMb5a9tq5RD1/LsOc7ExG1VX9KAWIsWOpojnlK92mQEOMZC', '{bcrypt}$2a$10$NlF1LONeXtejcNReYu3XPu37FJrNll691IdoUykpY89aUzgwlDlYK', 'ADMIN', 'STAND_BY', true, 1),
 (3, '3jeongg', 'leena0914@naver.com', '{bcrypt}$2a$10$ZyY8A2GMb5a9tq5RD1/LsOc7ExG1VX9KAWIsWOpojnlK92mQEOMZC', '{bcrypt}$2a$10$NlF1LONeXtejcNReYu3XPu37FJrNll691IdoUykpY89aUzgwlDlYK', 'ADMIN', 'STAND_BY', true, 2),
-(4, '4jeongg', 'leena0915@naver.com', '{bcrypt}$2a$10$ZyY8A2GMb5a9tq5RD1/LsOc7ExG1VX9KAWIsWOpojnlK92mQEOMZC', '{bcrypt}$2a$10$NlF1LONeXtejcNReYu3XPu37FJrNll691IdoUykpY89aUzgwlDlYK', 'SUPER_ADMIN', 'STAND_BY', true, 1);
+(4, '4jeongg', 'leena0915@naver.com', '{bcrypt}$2a$10$ZyY8A2GMb5a9tq5RD1/LsOc7ExG1VX9KAWIsWOpojnlK92mQEOMZC', '{bcrypt}$2a$10$NlF1LONeXtejcNReYu3XPu37FJrNll691IdoUykpY89aUzgwlDlYK', 'SUPER_ADMIN', 'STAND_BY', true, 1),
+(5, '5jeongg', 'leena0916@naver.com', '{bcrypt}$2a$10$ZyY8A2GMb5a9tq5RD1/LsOc7ExG1VX9KAWIsWOpojnlK92mQEOMZC', '{bcrypt}$2a$10$NlF1LONeXtejcNReYu3XPu37FJrNll691IdoUykpY89aUzgwlDlYK', 'ADMIN', 'ACTIVE', true, 1),
+(6, '6jeongg', 'leena0917@naver.com', '{bcrypt}$2a$10$ZyY8A2GMb5a9tq5RD1/LsOc7ExG1VX9KAWIsWOpojnlK92mQEOMZC', '{bcrypt}$2a$10$NlF1LONeXtejcNReYu3XPu37FJrNll691IdoUykpY89aUzgwlDlYK', 'USER', 'ACTIVE', true, 1);
 
-INSERT INTO company_image_tb (company_image_id, company_id, url) VALUES
-(1, 1, 'https://static.wixstatic.com/media/d465da_ed07388770a5418f8db3cf4e37573c47.jpg/v1/fill/w_342,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d465da_ed07388770a5418f8db3cf4e37573c47');
+INSERT INTO company_image_tb (`company_image_id`, `company_id`, `url`, `height`, `width`) VALUES
+(1, 1, 'https://static.wixstatic.com/media/d465da_ed07388770a5418f8db3cf4e37573c47.jpg/v1/fill/w_342,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d465da_ed07388770a5418f8db3cf4e37573c47', 1243, 56352);
+
+INSERT INTO router_tb (`router_id`, `company_id`, `mac_address`, `router_name`, `ssid`, `instance`, `job`, `position_x`, `position_y`) VALUES
+(1, 1, '5a:86:94:7f:b4:c7', '휴게실 Wifi', 'ipTIMEOpenWrt', '219.241.29.68:9100', 'routers', 124.32, 1653.22);
