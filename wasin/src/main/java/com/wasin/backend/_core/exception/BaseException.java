1de2d91a01c9a620d43e5509520bde2036219287
1jeongg <leena0912@pusan.ac.kr>
2024-08-07 20:10:14 +0900

    feat: add exception messages

--- wasin/src/main/java/com/wasin/backend/_core/exception/BaseException.java
+++ wasin/src/main/java/com/wasin/backend/_core/exception/BaseException.java
@@ -18,6 +18,9 @@ public enum BaseException {
     COMPANY_OPEN_API_FAIL("Open API를 이용하여 회사 목록을 불러오는데 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     AWS_IMAGE_FAIL("aws에 이미지를 올리는 것을 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_PATH("잘못된 url path입니다.", HttpStatus.NOT_FOUND),
+    MAX_FILE_SIZE("이미지 크기가 너무 큽니다.", HttpStatus.BAD_REQUEST),
+    FILE_READ_FAIL("파일을 읽을 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    ROUTER_PERMISSION_DENIED("회사가 다르므로 라우터에 대한 접근 권한이 없습니다.", HttpStatus.UNAUTHORIZED),
 
     // 유저
     USER_NOT_FOUND("서비스를 탈퇴했거나 가입하지 않은 유저의 요청입니다.", HttpStatus.NOT_FOUND),
@@ -27,6 +30,7 @@ public enum BaseException {
     USER_LOCK_PASSWORD_WRONG("잠금해제 패스워드를 잘못 입력하셨습니다.", HttpStatus.BAD_REQUEST),
     USER_PASSWORD_NOT_SAME("패스워드1과 패스워드2는 동일해야 합니다.", HttpStatus.BAD_REQUEST),
     USER_ROLE_WRONG("잘못된 역할입니다.", HttpStatus.BAD_REQUEST),
+    USER_INACTIVE("아직 활성화되지 않은 유저입니다.", HttpStatus.BAD_REQUEST),
 
     // 토큰
     ACCESS_TOKEN_NOT_FOUND("access token이 존재하지 않습니다." , HttpStatus.NOT_FOUND),
@@ -48,8 +52,18 @@ public enum BaseException {
     COMPANY_USER_NOT_FOUND("해당 관리자는 회사에 속해있지 않습니다.", HttpStatus.NOT_FOUND),
     NOT_SAME_COMPANY("일반 관리자와 최종 관리자의 회사가 다릅니다.", HttpStatus.BAD_REQUEST),
     WRONG_WASIN_SERVICE_KEY("와신 상담의 서비스키가 잘못되었습니다.", HttpStatus.BAD_REQUEST),
+    COMPANY_IMAGE_NOT_FOUND("회사 이미지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
+    // 라우터
+    ROUTER_NOT_EXIST("해당 MAC 주소를 가진 라우터가 프로메테우스 서버에 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    ROUTER_NOT_EXIST_IN_DB("라우터가 데이터베이스에 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    ROUTER_EXIST_IN_DB("라우터가 이미 데이터베이스에 존재합니다.", HttpStatus.BAD_REQUEST),
+    ROUTER_NOT_EXIST_IN_PROMETHEUS("라우터가 프로메테우스 서버에 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    PROMETHUS_SERVER_FAIL("프로메테우스 서버의 데이터를 불러오는데 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    GRAFANA_SERVER_FAIL("그라파나 서버의 데이터를 불러오는데 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 
     ;
+
 
     @Getter
     private final String message;
--- wasin/src/main/java/com/wasin/backend/_core/exception/GlobalExceptionHandler.java
+++ wasin/src/main/java/com/wasin/backend/_core/exception/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.reactive.resource.NoResourceFoundException;
 
 import java.sql.SQLException;
@@ -53,6 +54,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<?> noResourceFound() {
         BaseException e = BaseException.INVALID_PATH;
+        return response(error(e), e.getStatus());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<?> maxUploadExceeded() {
+        BaseException e = BaseException.MAX_FILE_SIZE;
         return response(error(e), e.getStatus());
     }
 
