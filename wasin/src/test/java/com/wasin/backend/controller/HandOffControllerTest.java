88205d5440d5837b738f28bacb4cf2441a82df55
1jeongg <leena0912@pusan.ac.kr>
2024-08-28 17:52:16 +0900

    test: test for hand off api controller

---
+++ wasin/src/test/java/com/wasin/backend/controller/HandOffControllerTest.java
@@ -0,0 +1,207 @@
+package com.wasin.backend.controller;
+
+import com.wasin.backend._core.exception.BaseException;
+import com.wasin.backend.domain.dto.HandOffRequest;
+import com.wasin.backend.util.TestModule;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@DisplayName("핸드오프 통합 테스트")
+public class HandOffControllerTest extends TestModule {
+
+    @Nested
+    @DisplayName("사용자 라우터 전체 리스트 조회")
+    class FindAll {
+
+        @DisplayName("성공")
+        @Test
+        @WithUserDetails("leena0917@naver.com")
+        public void success() throws Exception {
+            // given
+            HandOffRequest.UserRouterDTO requestDTO = new HandOffRequest.UserRouterDTO(
+                List.of(
+                    new HandOffRequest.RouterDTO("휴게실 wifi", "5a:86:94:7f:b4:c7", -60L),
+                    new HandOffRequest.RouterDTO("없는 wifi", "5c:90:94:7f:b4:c7", -70L)
+                )
+            );
+            String requestBody = om.writeValueAsString(requestDTO);
+
+            // when
+            ResultActions result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .post("/hand-off")
+                            .content(requestBody)
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            // then
+            result.andExpect(jsonPath("$.success").value("true"));
+            result.andExpect(jsonPath("$.response.isAuto").value("true"));
+            result.andExpect(jsonPath("$.response.routerList[0].isSystemExist").value("true"));
+            result.andExpect(jsonPath("$.response.routerList[1].isSystemExist").value("false"));
+        }
+
+        @DisplayName("실패")
+        @Test
+        @WithUserDetails("leena0917@naver.com")
+        public void fail() throws Exception {
+            // given
+            HandOffRequest.UserRouterDTO requestDTO = new HandOffRequest.UserRouterDTO(List.of());
+            String requestBody = om.writeValueAsString(requestDTO);
+
+            // when
+            ResultActions result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .post("/hand-off")
+                            .content(requestBody)
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            // then
+            result.andExpect(jsonPath("$.success").value("false"));
+        }
+    }
+
+    @Nested
+    @DisplayName("사용자 최적 라우터 조회")
+    class FindBestRouter {
+
+        @DisplayName("성공")
+        @Test
+        @WithUserDetails("leena0917@naver.com")
+        public void success() throws Exception {
+            // given
+            HandOffRequest.UserRouterDTO requestDTO = new HandOffRequest.UserRouterDTO(
+                    List.of(
+                            new HandOffRequest.RouterDTO("휴게실 wifi", "5a:86:94:7f:b4:c7", -60L),
+                            new HandOffRequest.RouterDTO("없는 wifi", "5c:90:94:7f:b4:c7", -70L)
+                    )
+            );
+            String requestBody = om.writeValueAsString(requestDTO);
+
+            // when
+            ResultActions result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .post("/hand-off/best")
+                            .content(requestBody)
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            // then
+            result.andExpect(jsonPath("$.success").value("true"));
+            result.andExpect(jsonPath("$.response.isAuto").value("true"));
+            result.andExpect(jsonPath("$.response.router.isSystemExist").value("true"));
+        }
+
+        @DisplayName("실패")
+        @Test
+        @WithUserDetails("leena0917@naver.com")
+        public void fail() throws Exception {
+            // given
+            HandOffRequest.UserRouterDTO requestDTO = new HandOffRequest.UserRouterDTO(List.of(
+                    new HandOffRequest.RouterDTO("", "", -60L)
+            ));
+            String requestBody = om.writeValueAsString(requestDTO);
+
+            // when
+            ResultActions result = mvc.perform(
+                    MockMvcRequestBuilders
+                            .post("/hand-off/best")
+                            .content(requestBody)
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            // then
+            result.andExpect(jsonPath("$.success").value("false"));
+        }
+    }
+
+    @Nested
+    @DisplayName("핸드오프 모드 자동으로 변경")
+    class ChangeModeAutoTest {
+
+        @DisplayName("성공")
+        @Test
+        @WithUserDetails("leena0920@naver.com")
+        public void success() throws Exception {
+            // given
+
+            // when
+            ResultActions result = mvc.perform(
+                    MockMvcRequestBuilders.post("/hand-off/mode/auto")
+            );
+            logResult(result);
+
+            // then
+            result.andExpect(jsonPath("$.success").value("true"));
+        }
+
+        @DisplayName("실패 - 이미 자동화된 유저")
+        @Test
+        @WithUserDetails("leena0917@naver.com")
+        public void fail() throws Exception {
+            // given
+
+            ResultActions result = mvc.perform(
+                    MockMvcRequestBuilders.post("/hand-off/mode/auto")
+            );
+            logResult(result);
+
+            // then
+            BaseException exception = BaseException.HANDOFF_ALREADY_AUTO;
+            result.andExpect(jsonPath("$.success").value("false"));
+            result.andExpect(jsonPath("$.error.status").value(exception.getStatus().value()));
+            result.andExpect(jsonPath("$.error.message").value(exception.getMessage()));
+
+        }
+    }
+
+    @Nested
+    @DisplayName("핸드오프 모드 수동으로 변경")
+    class ChangeModeManualTest {
+
+        @DisplayName("성공")
+        @Test
+        @WithUserDetails("leena0917@naver.com")
+        public void success() throws Exception {
+            // given
+
+            // when
+            ResultActions result = mvc.perform(
+                    MockMvcRequestBuilders.post("/hand-off/mode/manual")
+            );
+            logResult(result);
+
+            // then
+            result.andExpect(jsonPath("$.success").value("true"));
+        }
+
+        @DisplayName("실패 - 이미 수동화된 유저")
+        @Test
+        @WithUserDetails("leena0920@naver.com")
+        public void fail() throws Exception {
+            // given
+
+            ResultActions result = mvc.perform(
+                    MockMvcRequestBuilders.post("/hand-off/mode/manual")
+            );
+            logResult(result);
+
+            // then
+            BaseException exception = BaseException.HANDOFF_ALREADY_MANUAL;
+            result.andExpect(jsonPath("$.success").value("false"));
+            result.andExpect(jsonPath("$.error.status").value(exception.getStatus().value()));
+            result.andExpect(jsonPath("$.error.message").value(exception.getMessage()));
+
+        }
+    }
+}
