53e16b48a3fcfbc1f45c4a2c2001927236c241ae
1jeongg <leena0912@pusan.ac.kr>
2024-08-29 17:47:10 +0900

    feat: add port, serial number, password in router entity and dtos

--- wasin/src/main/java/com/wasin/backend/domain/dto/RouterRequest.java
+++ wasin/src/main/java/com/wasin/backend/domain/dto/RouterRequest.java
@@ -14,6 +14,18 @@ public class RouterRequest {
             @Size(max = 255, message = "라우터 MAC 주소는  255자 이내여야 합니다.")
             String macAddress,
 
+            @NotEmpty(message = "라우터 serialNumber는 비어있으면 안됩니다.")
+            @Size(max = 255, message = "라우터 serialNumber는  255자 이내여야 합니다.")
+            String serialNumber,
+
+            @NotEmpty(message = "라우터 password는 비어있으면 안됩니다.")
+            @Size(max = 255, message = "라우터 password는  255자 이내여야 합니다.")
+            String password,
+
+            @NotEmpty(message = "라우터 포트번호는 비어있으면 안됩니다.")
+            @Size(max = 255, message = "라우터 포트번호는  255자 이내여야 합니다.")
+            String port,
+
             @NotNull(message = "x축 위치는 비어있으면 안됩니다.")
             Double positionX,
 
@@ -26,6 +38,10 @@ public class RouterRequest {
             @NotEmpty(message = "라우터 이름은 비어있으면 안됩니다.")
             @Size(max = 255, message = "라우터 이름은 255자 이내여야 합니다.")
             String name,
+
+            @NotEmpty(message = "라우터 password는 비어있으면 안됩니다.")
+            @Size(max = 255, message = "라우터 password는  255자 이내여야 합니다.")
+            String password,
 
             @NotNull(message = "x축 위치는 비어있으면 안됩니다.")
             Double positionX,
