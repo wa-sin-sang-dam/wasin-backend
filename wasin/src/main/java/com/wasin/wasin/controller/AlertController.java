7223dab3f1c90ea217306ad36c12b0e3fe0722ea
1jeongg <leena0912@pusan.ac.kr>
2024-09-25 01:25:01 +0900

    chore: cd test

--- wasin/src/main/java/com/wasin/wasin/controller/AlertController.java
+++ wasin/src/main/java/com/wasin/wasin/controller/AlertController.java
@@ -5,10 +5,7 @@ import com.wasin.wasin.domain.dto.AlertRequest;
 import com.wasin.wasin.service.AlertService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +20,7 @@ public class AlertController {
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 
-    @PostMapping("/test")
+    @GetMapping("/test3")
     public ResponseEntity<?> test() {
 
         return ResponseEntity.ok().body(ApiUtils.success(null));
