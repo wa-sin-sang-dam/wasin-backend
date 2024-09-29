ca66f542cb3b6c943f90c1b05703bef7c0492191
1jeongg <leena0912@pusan.ac.kr>
2024-09-29 13:47:23 +0900

    feat: monitoring by monitorId api implemented

---
+++ wasin/src/main/java/com/wasin/wasin/_core/util/web_api/MonitoringApiUtil.java
@@ -0,0 +1,121 @@
+package com.wasin.wasin._core.util.web_api;
+
+import com.wasin.wasin._core.exception.BaseException;
+import com.wasin.wasin._core.exception.error.ServerException;
+import com.wasin.wasin.domain._const.Metric;
+import com.wasin.wasin.domain.dto.RouterResponse;
+import com.wasin.wasin.domain.entity.Router;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MonitoringApiUtil {
+
+    private final WebClient webClient;
+
+    @Value("${jang.header}")
+    String grafanaHeader;
+
+    @Value("${jang.datasource-uid}")
+    String grafanaDatasourceUID;
+
+    public RouterResponse.MonitorResult getMetric(Metric metric, Router router, Long time) {
+        String expr = switch (metric) {
+            case WIFI_SCORE -> getWifiScoreExpr(router);
+            case WIFI_CLIENT -> getWifiClientsExpr(router);
+            case CPU_SYSTEM -> getCPUSystemExpr(router);
+            case WIFI_NETWORK_NOISE -> getNetworkNoiseExpr(router);
+            case WIFI_BITRATE -> getWifiBitRateExpr(router);
+            case NETWORK_TRAFFIC_BY_BYTES -> getNetworkTrafficByBytesExpr(router);
+            case NETWORK_TRAFFIC_ERRORS -> getNetworkTrafficErrorsExpr(router);
+        };
+        RouterResponse.MonitoringQueries monitoringQuery = getMonitoringQuery(expr, time);
+        return getResult(monitoringQuery);
+    }
+
+
+    private String getWifiScoreExpr(Router router) {
+        return "scalar(1 - (avg(node_load1{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\"}) " +
+                "/ count(count(node_cpu_seconds_total{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\"}) by (cpu)))) " +
+                "* (1 - 0.3 * (wifi_network_bitrate{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\", ifname=\"wlan0\"} " +
+                "- min_over_time(wifi_network_bitrate{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\", ifname=\"wlan0\"}[3h])) " +
+                "/ (1 + max_over_time(wifi_network_bitrate{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\", ifname=\"wlan0\"}[3h]) " +
+                "- min_over_time(wifi_network_bitrate{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\", ifname=\"wlan0\"}[3h]))) * 100";
+    }
+
+    private String getWifiClientsExpr(Router router) {
+        return "sum(wifi_stations{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\"})";
+    }
+
+    private String getCPUSystemExpr(Router router) {
+        return "avg(node_load1{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\"}) / " +
+                " count(count(node_cpu_seconds_total{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\"}) by (cpu)) * 100";
+    }
+
+    private String getNetworkNoiseExpr(Router router) {
+        return "wifi_network_noise_dbm{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\"}";
+    }
+
+    private String getWifiBitRateExpr(Router router) {
+        return "wifi_network_bitrate{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\"}/1024";
+    }
+
+    private String getNetworkTrafficByBytesExpr(Router router) {
+        return "irate(node_network_receive_bytes_total{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() +  "\"}[5m])/1024";
+    }
+
+    private String getNetworkTrafficErrorsExpr(Router router) {
+        return "irate(node_network_receive_errs_total{instance=~\"" + router.getInstance() + "\",job=~\"" + router.getJob() + "\"}[5m])";
+    }
+
+    private RouterResponse.MonitorResult getResult(RouterResponse.MonitoringQueries requestDTO) {
+        try {
+            return Objects.requireNonNull(webClient.mutate()
+                            .build()
+                            .post()
+                            .uri("http://grafana.daily-cotidie.com/api/ds/query")
+                            .bodyValue(requestDTO)
+                            .header("Authorization", "Bearer " + grafanaHeader)
+                            .retrieve()
+                            .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(),
+                                    clientResponse -> clientResponse
+                                            .bodyToMono(String.class)
+                                            .map(body -> {
+                                                log.debug(body);
+                                                return new ServerException(BaseException.GRAFANA_REQUEST_FAIL);
+                                            }))
+                            .bodyToMono(RouterResponse.MonitorResult.class)
+                            .block());
+        } catch(Exception e) {
+            log.debug(e.getMessage());
+            throw new ServerException(BaseException.GRAFANA_SERVER_FAIL);
+        }
+    }
+
+    private RouterResponse.MonitoringQueries getMonitoringQuery(String expr, Long time) {
+        RouterResponse.Datasource datasource = new RouterResponse.Datasource("prometheus", grafanaDatasourceUID);
+        RouterResponse.MonitoringQuery query = new RouterResponse.MonitoringQuery(
+                datasource,
+                expr,
+                "time_series",
+                "A",
+                32400L,
+                1000L
+        );
+
+        return new RouterResponse.MonitoringQueries(
+                List.of(query),
+                "now-" + time + "m",
+                "now"
+        );
+    }
+
+}
---
+++ wasin/src/main/java/com/wasin/wasin/service/impl/MonitoringServiceImpl.java
@@ -0,0 +1,44 @@
+package com.wasin.wasin.service.impl;
+
+import com.wasin.wasin._core.exception.BaseException;
+import com.wasin.wasin._core.exception.error.NotFoundException;
+import com.wasin.wasin._core.util.web_api.MonitoringApiUtil;
+import com.wasin.wasin.domain._const.Metric;
+import com.wasin.wasin.domain.dto.MonitorResponse;
+import com.wasin.wasin.domain.dto.RouterResponse;
+import com.wasin.wasin.domain.entity.Router;
+import com.wasin.wasin.domain.entity.User;
+import com.wasin.wasin.domain.mapper.MonitoringMapper;
+import com.wasin.wasin.repository.RouterJPARepository;
+import com.wasin.wasin.service.MonitoringService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class MonitoringServiceImpl implements MonitoringService {
+
+    private final RouterJPARepository routerJPARepository;
+    private final MonitoringApiUtil monitoringApiUtil;
+    private final MonitoringMapper monitoringMapper;
+
+    public MonitorResponse.FindById monitorById(Long routerId, Long metricId, Long time, User user) {
+        // default values
+        if (metricId == null) metricId = 0L;
+        if (time == null) time = 60L;
+
+        Metric metric = Metric.findByMetricId(metricId);
+        Router router = findByRouterId(routerId);
+        RouterResponse.MonitorResult results = monitoringApiUtil.getMetric(metric, router, time);
+
+        return monitoringMapper.resultToDTO(results, metricId, time);
+    }
+
+    private Router findByRouterId(Long routerId) {
+        return routerJPARepository.findById(routerId).orElseThrow(
+                () -> new NotFoundException(BaseException.ROUTER_NOT_EXIST_IN_DB)
+        );
+    }
+}
