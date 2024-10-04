314d2c80a977549244821caca4adda38a55fab13
1jeongg <leena0912@pusan.ac.kr>
2024-10-04 23:04:45 +0900

    feat: add permission check for monitoring

--- wasin/src/main/java/com/wasin/wasin/service/impl/MonitoringServiceImpl.java
+++ wasin/src/main/java/com/wasin/wasin/service/impl/MonitoringServiceImpl.java
@@ -6,31 +6,40 @@ import com.wasin.wasin._core.util.web_api.MonitoringApiUtil;
 import com.wasin.wasin.domain._const.Metric;
 import com.wasin.wasin.domain.dto.MonitorResponse;
 import com.wasin.wasin.domain.dto.RouterResponse;
+import com.wasin.wasin.domain.entity.Company;
 import com.wasin.wasin.domain.entity.Router;
 import com.wasin.wasin.domain.entity.User;
 import com.wasin.wasin.domain.mapper.MonitoringMapper;
+import com.wasin.wasin.domain.validation.MonitoringValidation;
 import com.wasin.wasin.repository.RouterJPARepository;
+import com.wasin.wasin.repository.UserJPARepository;
 import com.wasin.wasin.service.MonitoringService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class MonitoringServiceImpl implements MonitoringService {
 
+    private final UserJPARepository userJPARepository;
     private final RouterJPARepository routerJPARepository;
     private final MonitoringApiUtil monitoringApiUtil;
     private final MonitoringMapper monitoringMapper;
+    private final MonitoringValidation monitoringValidation;
 
-    public MonitorResponse.FindById monitorById(Long routerId, Long metricId, Long time, User user) {
-        // default values
+    public MonitorResponse.FindById monitorById(Long routerId, Long metricId, Long time, User _user) {
         if (metricId == null) metricId = 0L;
         if (time == null) time = 60L;
 
         Metric metric = Metric.findByMetricId(metricId);
         Router router = findByRouterId(routerId);
+        User user = findUserByUserId(_user.getId());
+
+        monitoringValidation.checkRouterExistInGroup(router, user);
         RouterResponse.MonitorResult results = monitoringApiUtil.getMetric(metric, router, time);
 
         return monitoringMapper.resultToDTO(results, metricId, time);
